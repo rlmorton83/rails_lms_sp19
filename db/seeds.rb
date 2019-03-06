f8604a374338b1bda1ac486742addd52659360f7
Robert Morton <robert_morton@Roberts-MacBook-Pro.local>
2019-03-05 18:52:44 -0700

    finished lms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+roles = ['teacher', 'ta', 'student']
+10.times do
+    course = Course.create(
+        name: Faker::ChuckNorris.fact
+    )
+    
+10.times do
+user = User.create(
+first_name: Faker::Name.first_name,
+last_name: Faker::Name.last_name
+
+Enrollment.create(
+role: roles.sample,
+course_id: coourse.id,
+user_id: user.id
+
+)
+)
+
+
+
+end
+
+puts 'All the data is seeded'
+
+
+
+
